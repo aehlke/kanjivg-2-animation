919ebc49c3d10ee89429a20aacd36b5071e1cb48
Alex Ehlke <alex.ehlke@gmail.com>
2016-07-14 18:17:35 -0400

    pep8

--- kanjivg2animation.py
+++ kanjivg2animation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 from __future__ import (
     print_function,
@@ -14,7 +14,7 @@ from svg.path import parse_path
 from tqdm import tqdm
 
 
-#A function that allows you to retrieve a string between two specified strings
+# A function that allows you to retrieve a string between two specified strings
 def find_between(s, first, last):
     try:
         start = s.index( first ) + len( first )
@@ -27,48 +27,52 @@ def find_between(s, first, last):
 KANJIVG_SVG_DIR = './kanjivg/kanji/'
 
 
-#Iterate through each svg file in the kanjivg directory.
-for filename in tqdm(os.listdir(KANJIVG_SVG_DIR), mininterval=5, miniters=10):
-    #Our source and target files and directories
+# Iterate through each svg file in the kanjivg directory.
+for filename in tqdm(os.listdir(KANJIVG_SVG_DIR),
+                     mininterval=5, miniters=10):
+    # Our source and target files and directories
     source_file = open(KANJIVG_SVG_DIR + filename, 'r', encoding='utf8')
-    target_file = open('./converted/' + filename[:-4] + '-jlect.svg', 'w+', encoding='utf8')
+    target_file = open('./converted/{}-animated.svg'.format(filename[:-4]),
+                       'w+', encoding='utf8')
 
-    #The array that we will use to build the various parts comprising the svg
+    # The array that we will use to build the various parts comprising the svg
     svg_build_array = []
 
-    #The array that will contain all values of 'd' (the path code) in the original svg file
+    # The array that will contain all values of 'd' (the path code) in the original svg file
     dpath = []
 
-    #Retrieve the value of 'd' (the path code) in the original svg file
+    # Retrieve the value of 'd' (the path code) in the original svg file
     for line in source_file:
-        if "<path" in line and "d=\"" in line:
-            dpath.append(find_between(line, " d=\"", "\"/>"))
+        if '<path' in line and 'd="' in line:
+            dpath.append(find_between(line, ' d="', '\/>'))
 
     source_file.close()
 
-    #Start building an array that contains the fragments of our svg
-    svg_build_array.append("<svg id=\"kvg-" + filename[:-4] + "\" class=\"kanjivg\" width=\"106\" height=\"126\" ")
+    # Start building an array that contains the fragments of our svg
+    svg_build_array.append(
+        '<svg id="kvg-{}" class="kanjivg" width="106" height="126" '.format(
+            filename[:-4]))
 
-    ##Thought: maybe make end animation last longer before repeat? Or maybe keep it permanent?
+    # #Thought: maybe make end animation last longer before repeat? Or maybe keep it permanent?
 
-    #Begin defining the svg, its components and their styles
-    svg_build_array.append("""xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" version="1.1" baseProfile="full"><defs><style type="text/css"><![CDATA[path.black{fill:none;stroke:black;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}path.grey{fill:none;stroke:#ddd;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}path.stroke{fill:none;stroke:black;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}rect{fill:none;stroke:#555;stroke-width:2;} text{font-size:16px;font-family:Segoe UI Symbol,Cambria Math,DejaVu Sans,Symbola,Quivira,STIX,Code2000;-webkit-touch-callout:none;cursor:pointer;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select: none;}text:hover{color:#777;}#reset{font-weight:bold;}]]></style><marker id="markerStart" markerWidth="8" markerHeight="8" style="overflow:visible;"><circle cx="0" cy="0" r="1.5" style="stroke:none;fill:red;fill-opacity:0.5;"/></marker><marker id="markerEnd" style="overflow:visible;"><circle cx="0" cy="0" r="0.8" style="stroke:none;fill:blue;fill-opacity:0.5;"><animate attributeName="opacity" from="1" to="0" dur="3s" repeatCount="indefinite" /></circle></marker></defs>""")
+    # Begin defining the svg, its components and their styles
+    svg_build_array.append("""xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" version="1.1" baseProfile="full"><defs><style type="text/css"><![CDATA[path.black{fill:none;stroke:black;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}path.grey{fill:none;stroke:# ddd;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}path.stroke{fill:none;stroke:black;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;}rect{fill:none;stroke:#555;stroke-width:2;} text{font-size:16px;font-family:Segoe UI Symbol,Cambria Math,DejaVu Sans,Symbola,Quivira,STIX,Code2000;-webkit-touch-callout:none;cursor:pointer;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select: none;}text:hover{color:#777;}#reset{font-weight:bold;}]]></style><marker id="markerStart" markerWidth="8" markerHeight="8" style="overflow:visible;"><circle cx="0" cy="0" r="1.5" style="stroke:none;fill:red;fill-opacity:0.5;"/></marker><marker id="markerEnd" style="overflow:visible;"><circle cx="0" cy="0" r="0.8" style="stroke:none;fill:blue;fill-opacity:0.5;"><animate attributeName="opacity" from="1" to="0" dur="3s" repeatCount="indefinite" /></circle></marker></defs>""")
 
     svg_build_array.append('<rect x="4" y="4" width="100" height="100" />')
 
-    #First we draw the grey kanji strokes in the background, without any animations
+    # First we draw the grey kanji strokes in the background, without any animations
     for a in dpath:
         svg_build_array.append('<path d="' + a + '" class="grey" />')
 
-    #The value of i represents
+    # The value of i represents
     i = 0.0
 
-    #Then we handle the black kanji strokes, which will be animated in the foreground
+    # Then we handle the black kanji strokes, which will be animated in the foreground
     for b in dpath:
         path_length = parse_path(b).length()
         stroke_length = 150
 
-        #Change the stroke length if the path length exceeds our expectations
+        # Change the stroke length if the path length exceeds our expectations
         if path_length > 150 and path_length < 200:
             stroke_length = 200
         elif path_length > 200  and path_length < 250:
@@ -76,20 +80,24 @@ for filename in tqdm(os.listdir(KANJIVG_SVG_DIR), mininterval=5, miniters=10):
         elif path_length > 250:
             stroke_length = 300
 
-        #Begin handling the path
-        svg_build_array.append('<path d="' + b + '" class="stroke" stroke-dasharray="' + str(stroke_length) + '">')
+        # Begin handling the path
+        svg_build_array.append(
+            '<path d="{}" class="stroke" stroke-dasharray="{}">'.format(
+                b, stroke_length))
 
-        #Hide the black stroke for a specified duration (dur)
+        # Hide the black stroke for a specified duration ("dur")
         if i is not 0:
-            svg_build_array.append('<set attributeName="opacity" to="0" dur="' + str(i) + 's" />')
+            svg_build_array.append(
+                '<set attributeName="opacity" to="0" dur="{}s" />'.format(i))
 
-        #Animate the black stroke after a specified duration (begin)
+        # Animate the black stroke after a specified duration (begin)
         svg_build_array.append('<animate attributeName="stroke-dashoffset" from="' + str(stroke_length) + '" to="0" dur="1.8s" begin="' + str(i) + 's" fill="freeze" />')
 
-        #Finish handling the path
+        # Finish handling the path
         svg_build_array.append('</path>')
 
-        #Given the length of the current path, calculate the amount of time we should add to 'i'. The next path will animate after that time.
+        # Given the length of the current path, calculate the amount of time
+        # we should add to 'i'. The next path will animate after that time.
         MIN_DELAY = 0.3
         MAX_DELAY = 1.0
         SHORT_PATH_LENGTH = 20
@@ -100,15 +108,16 @@ for filename in tqdm(os.listdir(KANJIVG_SVG_DIR), mininterval=5, miniters=10):
             (MAX_DELAY - MIN_DELAY)
         )
 
-    #Finish handling the svg
+    # Finish handling the svg
     svg_build_array.append('</svg>')
 
-    #Write the svg build array to a file
+    # Write the svg build array to a file
     for item in svg_build_array:
         target_file.write(item)
 
-    #Close the file
+    # Close the file
     target_file.close()
 
-#When the script is finished converting all files in the specified directory, it will print the following message to indicate it has finished.
+# When the script is finished converting all files in the specified directory,
+# it will print the following message to indicate it has finished.
 print("Done.")
